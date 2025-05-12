5221c0ed730295323b75276e00e8135c155f9aac
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-05-12 14:34:10 -0400

    spelling: authentication
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- lib/CPANPLUS/Internals/Source/CPANMetaDB/HTTP.pm
+++ lib/CPANPLUS/Internals/Source/CPANMetaDB/HTTP.pm
@@ -1030,7 +1030,7 @@ setting.  Usual choices are GET, POST, PUT, HEAD
 
     - FTP 
     - HTTPS (SSL)
-    - Authenitcation/Authorizaton/Proxy-Authorization
+    - Authentication/Authorizaton/Proxy-Authorization
       are not directly supported, and require MIME::Base64.
     - Redirects (Location) are not automatically followed
     - multipart/form-data POSTs are not directly supported (necessary
